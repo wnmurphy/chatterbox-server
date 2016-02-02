39f37d15cab106e6ba12f87ae6afaa43117f5e48
Neil Murphy <wnmurphy@gmail.com>
2016-02-02 12:00:42 -0800

    Server behaving as expected.

--- client/env/config.js
+++ client/env/config.js
@@ -10,9 +10,3 @@ if (!/(&|\?)username=/.test(window.location.search)) {
   newSearch += 'username=' + (prompt('What is your name?') || 'anonymous');
   window.location.search = newSearch;
 }
-
-// Put your parse application keys here!
-$.ajaxPrefilter(function (settings, _, jqXHR) {
-  jqXHR.setRequestHeader("X-Parse-Application-Id", "wPfavNOxOKTzlXiMq9r226nSo5Vl5aMFfEcvAupE");
-  jqXHR.setRequestHeader("X-Parse-REST-API-Key", "eCGqXWtDjoZofGzf785KvSODrUrXURbL6JCQqaHj");
-});
--- server/request-handler.js
+++ server/request-handler.js
@@ -4,7 +4,8 @@ exports.requestHandler = function(request, response) {
   console.log("Serving request type " + request.method + " for url " + request.url);
   
   var pathName = url.parse(request.url).pathname.split('/');
-  
+  var query = url.parse(request.url, true).query;
+
   if(pathName[1] === 'classes'){
 
     if(request.method === "GET"){
@@ -18,8 +19,10 @@ exports.requestHandler = function(request, response) {
       if(!Array.isArray(data[pathName[2]])){
         data[pathName[2]] = [];
       }
-                  
-      response.end(JSON.stringify( {results: data[pathName[2]] } ));
+
+      var results = filterByTime(data[pathName[2]], query);
+      
+      response.end(JSON.stringify( {results: results } ));
 
     } else if(request.method === "POST"){
         
@@ -33,10 +36,22 @@ exports.requestHandler = function(request, response) {
         if(!Array.isArray(data[pathName[2]])){
           data[pathName[2]] = [];
         }
-        data[pathName[2]].push(JSON.parse(newData.toString()));
+        var newMessage = JSON.parse(newData.toString());
+        newMessage.createdAt = Date.now();
+        data[pathName[2]].push(newMessage);
+
       });
     
       response.end(); 
+
+    } else if(request.method === "OPTIONS"){
+      
+      statusCode = 200;
+      headers = defaultCorsHeaders;
+      headers['Content-Type'] = "application/json";
+      response.writeHead(statusCode, headers);
+      response.end();
+      
     }
     
   } else {
@@ -48,6 +63,54 @@ exports.requestHandler = function(request, response) {
   }
 };
 
+var filterByTime = function(array, query){
+  var filterObject;
+  // If where is not empty, pull out '{"$gt":1454438415229}}',
+  if(query.where !== ''){  
+    filterObject = JSON.parse(query.where).createdAt;
+    var limit = Number.parseInt(query.limit);
+    var direction = null;
+    var timeStamp = null;
+    
+    // Get direction and timeStamp from filterObject
+    for(var key in filterObject){
+      direction = key;
+      timeStamp = filterObject[key];
+    }
+    
+    // filter messages by direction
+    var timeIndex = binarySearch(array, timeStamp);
+    
+    if(direction === '$gt') {
+      return array.slice(timeIndex+1, timeIndex + limit + 1);
+    }else{
+      return array.slice(timeIndex-limit, timeIndex);
+    }
+  } else {
+    return array.slice(array.length - Number.parseInt(query.limit) - 1);
+  }
+
+};
+
+var binarySearch = function (array, target, start, end) {
+  if(array.length < 1){return;}
+  start = start || 0;
+  end = end || array.length - 1;
+  var index = start + Math.floor((end-start)/2);
+
+  if (array[index].createdAt === target) {
+    return index;
+  }
+  if (end === start) {
+    return -1; 
+  }
+  if (target < array[index].createdAt) {
+    return binarySearch(array, target, start, index - 1);
+  } else {
+    return binarySearch(array, target, index + 1, end);
+  }
+}
+
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
@@ -55,4 +118,9 @@ var defaultCorsHeaders = {
   "access-control-max-age": 10 // Seconds.
 };
 
-var data = {};
+var data = {
+  messages: [ { text: 'josh',
+    username: 'Josh',
+    roomname: 'showAll',
+    createdAt: 1454442295 } ]
+};
